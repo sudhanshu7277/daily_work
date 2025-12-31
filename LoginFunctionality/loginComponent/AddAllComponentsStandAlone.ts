8fc3380aa57080650e6d58db29574d717ec07346
Sudhanshu <sudhanshuj020@gmail.com>
2025-12-31 12:52:24 -0500

    removed old pushed files

--- LoginFunctionality/loginComponent/AddAllComponentsStandAlone.ts
+++
@@ -1,8 +0,0 @@
-@Component({
-    selector: 'app-example',
-    standalone: true, // ← Required
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule, AgGridModule /* other needed modules or standalone components */],
-    templateUrl: './example.component.html',
-    styleUrls: ['./example.component.scss']
-  })
-  export class ExampleComponent { /* ... */ }
