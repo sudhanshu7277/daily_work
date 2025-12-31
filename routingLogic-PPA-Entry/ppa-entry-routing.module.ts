63bead1d6a5485d086eaccc41a2fe0c93fa6ff57
Sudhanshu <sudhanshuj020@gmail.com>
2025-12-31 08:23:36 -0500

    updated ppa routing module

--- routingLogic-PPA-Entry/ppa-entry-routing.module.ts
+++ routingLogic-PPA-Entry/ppa-entry-routing.module.ts
@@ -1,26 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PpaEntryComponent } from './ppa-entry.component';
-
-// Import your child components (all standalone except possibly some)
 import { InputComponent } from './components/input/input.component';
 import { Checker1Component } from './components/checker1/checker1.component';
 import { Checker2Component } from './components/checker2/checker2.component';
-// Add more as needed (Checker3, Checker4, etc.)
+// ... other components
 
 const routes: Routes = [
   {
     path: '',
-    component: PpaEntryComponent,  // Parent component with tabs
+    component: PpaEntryComponent,
     children: [
       { path: '', redirectTo: 'input', pathMatch: 'full' },
-      { path: 'input', component: InputComponent },
-      { path: 'checker1', component: Checker1Component },
-      { path: 'checker2', component: Checker2Component },
-      // Add more routes as you create components
-      // { path: 'checker3', component: Checker3Component },
-      // { path: 'checker4', component: Checker4Component },
-      // { path: 'resubmit', component: ResubmitComponent },
+      {
+        path: 'input',
+        component: InputComponent,
+        data: { screen: 'Input' }
+      },
+      {
+        path: 'checker1',
+        component: Checker1Component,
+        data: { screen: 'Checker1' },
+        canActivate: [RoleGuard]
+      },
+      {
+        path: 'checker2',
+        component: Checker2Component,
+        data: { screen: 'Checker2' },
+        canActivate: [RoleGuard]
+      },
+      // Add more with data: { screen: 'Checker3' }, etc.
     ]
   }
 ];
@@ -29,8 +38,7 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class PpaEntryRoutingModule { }
-
+export class PpaEntryRoutingModule {}
 
 
 
@@ -45,25 +53,19 @@ export class PpaEntryRoutingModule { }
 // import { NgModule } from '@angular/core';
 // import { RouterModule, Routes } from '@angular/router';
 // import { PpaEntryComponent } from './ppa-entry.component';
-
-// // Import or create your tab-specific components
-// import { InputComponent } from './input/input.component';           // Default "Input" tab
-// import { Checker1Component } from './checker1/checker1.component';
-// import { Checker2Component } from './checker2/checker2.component';
-// // ... add Checker3Component, Checker4Component, ResubmitComponent, etc.
+// import { InputComponent } from './components/input/input.component';
+// import { Checker1Component } from './components/checker1/checker1.component';
+// import { Checker2Component } from './components/checker2/checker2.component';
 
 // const routes: Routes = [
 //   {
 //     path: '',
-//     component: PpaEntryComponent,  // This component has the tabs + outlet
+//     component: PpaEntryComponent,
 //     children: [
 //       { path: '', redirectTo: 'input', pathMatch: 'full' },
 //       { path: 'input', component: InputComponent },
-//       { path: 'checker-1', component: Checker1Component },
-//       { path: 'checker-2', component: Checker2Component },
-//       // { path: 'checker-3', component: Checker3Component },
-//       // { path: 'checker-4', component: Checker4Component },
-//       // { path: 'resubmit', component: ResubmitComponent },
+//       { path: 'checker1', component: Checker1Component },
+//       { path: 'checker2', component: Checker2Component },
 //     ]
 //   }
 // ];
