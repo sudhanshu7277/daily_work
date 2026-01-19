80f8be999344ca2a92455e7e7c9a4058f429cc4f
sudhanshu7277 <sudhanshuj020@gmail.com>
2026-01-19 18:34:34 -0500

    Delete Montrial/bmo-legal-hold-dev directory
    Deleting bmo-legal-hold-dev

--- Montrial/bmo-legal-hold-dev/src/app/features/legal-hold/legal-hold.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { LegalHoldShellComponent } from './legal-hold-shell.component';
-
-export const LEGAL_HOLD_ROUTES: Routes = [
-  { path: '', component: LegalHoldShellComponent }
-];
