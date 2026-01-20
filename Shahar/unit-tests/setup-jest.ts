da061c7e548b3d19abccd94032a8c7317e82e49b
Sudhanshu <sudhanshuj020@gmail.com>
2026-01-19 22:00:32 -0500

    setup jest

--- Shahar/unit-tests/setup-jest.ts
+++ Shahar/unit-tests/setup-jest.ts
@@ -1,10 +1,23 @@
-import 'jest-preset-angular/setup-jest';
+// import 'jest-preset-angular/setup-jest';
 
-// Global mock for Ag-Grid (if needed)
-Object.defineProperty(window, 'getComputedStyle', {
-  value: () => ({
-    getPropertyValue: (prop: string) => {
-      return '';
-    }
-  })
-});
+// // Global mock for Ag-Grid (if needed)
+// Object.defineProperty(window, 'getComputedStyle', {
+//   value: () => ({
+//     getPropertyValue: (prop: string) => {
+//       return '';
+//     }
+//   })
+// });
+
+
+import { TestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+// This initializes the environment for all 18 of your test suites
+TestBed.initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
