bd727b923dccdcbecba7fd6d797314df9cdca698
Sudhanshu <sudhanshuj020@gmail.com>
2026-01-21 15:44:53 -0500

    search customer component, individual/ entity functionality

--- Montrial/src/app/features/legal-hold/components/search-customer/search-customer.component.ts
+++ Montrial/src/app/features/legal-hold/components/search-customer/search-customer.component.ts
@@ -48,15 +48,42 @@ export class SearchCustomerComponent implements OnInit{
   ngOnInit(): void {
     this.searchForm = this.fb.group({
       type: ['individual'],
+      // Individual fields
       lastName: ['', Validators.required],
       middleName: [''],
       firstName: ['', Validators.required],
       dob: [null],
+      // Entity fields
+      entityName: [''],
+      // Shared fields
       phone: [''],
       email: ['', Validators.email],
       lawyerEmail: ['', Validators.email],
       manager: ['']
     });
+
+    this.searchForm.get('type')?.valueChanges.subscribe(value => {
+      this.updateValidators(value);
+    });
+  }
+
+  private updateValidators(type: string): void {
+    const last = this.searchForm.get('lastName');
+    const first = this.searchForm.get('firstName');
+    const entity = this.searchForm.get('entityName');
+
+    if (type === 'individual') {
+      last?.setValidators(Validators.required);
+      first?.setValidators(Validators.required);
+      entity?.clearValidators();
+    } else {
+      last?.clearValidators();
+      first?.clearValidators();
+      entity?.setValidators(Validators.required);
+    }
+
+    // Refresh validation state for all affected fields
+    [last, first, entity].forEach(control => control?.updateValueAndValidity());
   }
 
   isFormValid(): boolean {
@@ -75,3 +102,88 @@ export class SearchCustomerComponent implements OnInit{
     this.clearTriggered.emit();
   }
 }
+
+
+
+
+
+
+
+
+// import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatRadioModule } from '@angular/material/radio';
+// import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatNativeDateModule } from '@angular/material/core';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatButtonModule } from '@angular/material/button';
+
+// @Component({
+//   selector: 'app-search-customer',
+//   standalone: true,
+//   imports: [
+//     CommonModule, 
+//     ReactiveFormsModule,
+//     MatInputModule,
+//     MatFormFieldModule,
+//     MatRadioModule,
+//     MatDatepickerModule,
+//     MatNativeDateModule,
+//     MatIconModule,
+//     MatButtonModule
+//   ],
+//   templateUrl: './search-customer.component.html',
+//   styleUrls: ['./search-customer.component.scss']
+// })
+// export class SearchCustomerComponent implements OnInit{
+//   @Output() searchTriggered = new EventEmitter<any>();
+//   @Output() clearTriggered = new EventEmitter<void>();
+//   searchForm!: FormGroup;
+
+//   constructor(private fb: FormBuilder) {}
+  
+//   criteria = {
+//     type: 'individual',
+//     firstName: '',
+//     lastName: '',
+//     middleName: '',
+//     dob: '',
+//     phone: '',
+//     email: '',
+//     lawyerEmail: '',
+//     manager: ''
+//   };
+
+//   ngOnInit(): void {
+//     this.searchForm = this.fb.group({
+//       type: ['individual'],
+//       lastName: ['', Validators.required],
+//       middleName: [''],
+//       firstName: ['', Validators.required],
+//       dob: [null],
+//       phone: [''],
+//       email: ['', Validators.email],
+//       lawyerEmail: ['', Validators.email],
+//       manager: ['']
+//     });
+//   }
+
+//   isFormValid(): boolean {
+//     return this.criteria.firstName.trim().length > 0 && 
+//            this.criteria.lastName.trim().length > 0;
+//   }
+
+//   onSearch(): void {
+//     if (this.searchForm.valid) {
+//       this.searchTriggered.emit(this.searchForm.value);
+//     }
+//   }
+
+//   onClear(): void {
+//     this.searchForm.reset({ type: 'individual' });
+//     this.clearTriggered.emit();
+//   }
+// }
