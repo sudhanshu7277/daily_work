5e80709c7a62b7bee30fe28ff48ec48b00e8ca10
Sudhanshu <sudhanshuj020@gmail.com>
2026-04-04 19:47:03 -0400

    npmrcs for maker and checker packages

---
+++ Shahar/npmrcs/checker-npmrc.ts
@@ -0,0 +1,13 @@
+# .npmrc — payment-checker
+# Registry: Citigroup Artifactory npm-teamdev
+registry=https://www.artifactrepository.citigroup.net/artifactory/api/npm/npm-teamdev/
+//www.artifactrepository.citigroup.net/artifactory/api/npm/npm-teamdev/:_authToken=${NPM_AUTH_TOKEN}
+
+strict-ssl=false
+email=ss71872@citi.com
+always-auth=true
+cafile=C:\Users\ss71872\cacerts.jks
+prefix=C:\Users\ss71872\AppData\Roaming\npm
+
+save-exact=true
+legacy-peer-deps=true
