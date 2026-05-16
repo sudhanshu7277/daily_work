de7ea64e41e9ce4cd832b0af813393a0b995e6fc
Sudhanshu <sudhanshuj020@gmail.com>
2026-05-16 17:02:03 -0400

    fixing capturingRadioButtonClick function

--- Montrial/radio-button-group.ts
+++ Montrial/radio-button-group.ts
@@ -44,3 +44,30 @@ submitActiveTasks() {
       }
     }
   }
+
+
+  capturingRadioButtonClick(event: any) {
+    if (!this.tasksForm.value.taskArray) {
+      this.tasksForm.value.taskArray = [];
+    }
+  
+    // 1. Get the dynamic key name from the incoming event (e.g., 'declarationOfHeirship')
+    const incomingKey = Object.keys(event)[0];
+  
+    // 2. Find if this task key already exists in your pre-populated array
+    const existingIndex = this.tasksForm.value.taskArray.findIndex(
+      (item: any) => item && item[incomingKey] !== undefined
+    );
+  
+    if (existingIndex !== -1) {
+      // 3. Update the existing object's value in place
+      this.tasksForm.value.taskArray[existingIndex][incomingKey] = event[incomingKey];
+    } else {
+      // Fallback: If it wasn't pre-populated, push it
+      this.tasksForm.value.taskArray.push(event);
+    }
+  
+    // 4. Ensure Angular marks the form tree statuses properly
+    this.tasksForm.markAsDirty();
+    this.tasksForm.markAsTouched();
+  }
