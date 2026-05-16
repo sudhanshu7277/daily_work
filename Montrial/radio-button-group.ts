9fe8f6981382b40dcba35d1428ccad29ff68a066
Sudhanshu <sudhanshuj020@gmail.com>
2026-05-16 17:14:24 -0400

    submitActiveTasks func updated

--- Montrial/radio-button-group.ts
+++ Montrial/radio-button-group.ts
@@ -1,50 +1,3 @@
-submitActiveTasks() {
-    if (this.tasksForm.dirty && this.tasksForm.touched) {
-      console.log('this.tasksForm values : ', this.tasksForm.value);
-      
-      let hasTasks = false;
-      const isNewCase = this.caseVersion !== null;
-  
-      // Determine the correct Enum set based on caseVersion rule
-      const COMPLETE_STATE = isNewCase ? TaskStateNewCases.COMPLETE : TaskState.COMPLETE;
-  
-      const eCETaskBody = {
-        name: this.categoryName,
-        tasks: this.tasks
-          .filter((task) => {
-            // Find the interaction event by matching the task name key dynamically
-            const taskEvent = this.tasksForm.value.taskArray?.find((t: any) => t && t[task.name] !== undefined);
-            return taskEvent !== undefined;
-          })
-          .map((task) => {
-            hasTasks = true;
-  
-            // Safely extract the dynamically set form state by task name matching
-            const taskEvent = this.tasksForm.value.taskArray.find((t: any) => t && t[task.name] !== undefined);
-            const formState = taskEvent[task.name];
-  
-            return {
-              ...task,
-              isIrregular:
-                task.isIrregular === TaskIrregular.IRREGULAR &&
-                formState !== task.state &&
-                formState === COMPLETE_STATE
-                  ? TaskIrregular.CORRECTED
-                  : task.isIrregular,
-              state: formState, // Resolves to 'na', 'uploaded', 'complete', etc. safely
-            };
-          })
-      };
-  
-      if (hasTasks) {
-        this.submitTasks.emit({
-          eCETaskBody,
-          isUpdate: true,
-        });
-      }
-    }
-  }
-
 
   capturingRadioButtonClick(event: any) {
     if (!this.tasksForm.value.taskArray) {
@@ -70,4 +23,55 @@ submitActiveTasks() {
     // 4. Ensure Angular marks the form tree statuses properly
     this.tasksForm.markAsDirty();
     this.tasksForm.markAsTouched();
+  }
+
+
+
+  // updated submitActiveTasks function
+
+  submitActiveTasks() {
+    if (this.tasksForm.dirty && this.tasksForm.touched) {
+      console.log('this.tasksForm values : ', this.tasksForm.value);
+      
+      let hasTasks = false;
+      const isNewCase = this.caseVersion !== null;
+  
+      // 1. Enforce CaseVersion Rule for Complete State check
+      const COMPLETE_STATE = isNewCase ? TaskStateNewCases.COMPLETE : TaskState.COMPLETE;
+  
+      const eCETaskBody = {
+        name: this.categoryName,
+        tasks: this.tasks
+          .filter((task) => {
+            // Find the form interaction object by matching its specific key name string
+            const taskEvent = this.tasksForm.value.taskArray?.find((t: any) => t && t[task.name] !== undefined);
+            return taskEvent !== undefined;
+          })
+          .map((task) => {
+            hasTasks = true;
+  
+            // Extract the form value using a safe name-lookup instead of an index loop [i]
+            const taskEvent = this.tasksForm.value.taskArray.find((t: any) => t && t[task.name] !== undefined);
+            const formState = taskEvent[task.name];
+  
+            return {
+              ...task,
+              isIrregular:
+                task.isIrregular === TaskIrregular.IRREGULAR &&
+                formState !== task.state &&
+                formState === COMPLETE_STATE
+                  ? TaskIrregular.CORRECTED
+                  : task.isIrregular,
+              state: formState, // Resolves safely to 'na', 'uploaded', etc.
+            };
+          })
+      };
+  
+      if (hasTasks) {
+        this.submitTasks.emit({
+          eCETaskBody,
+          isUpdate: true,
+        });
+      }
+    }
   }
