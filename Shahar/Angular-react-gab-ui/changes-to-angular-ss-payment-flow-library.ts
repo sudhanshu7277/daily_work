2791e4846f761e352c3a7bd3bcc803b716b8f71d
Sudhanshu <sudhanshuj020@gmail.com>
2026-06-02 00:42:11 -0400

    angular json, absolute path

--- Shahar/Angular-react-gab-ui/changes-to-angular-ss-payment-flow-library.ts
+++ Shahar/Angular-react-gab-ui/changes-to-angular-ss-payment-flow-library.ts
@@ -168,3 +168,6 @@ import { SSPaymentMakerComponent } from '../public-api';
     ]
   }
 
+// angular.json
+
+"outputPath": "C:/Users/SJ81534/Documents/SS-PAYMENT-UI--ACTUAL-LIBRARY/179025.shared-services.payment-flow-ui-lib/dist/element"
