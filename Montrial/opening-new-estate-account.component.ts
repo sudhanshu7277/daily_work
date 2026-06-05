24d763555775b022bd22d5af0b59c7908e17d6d0
Sudhanshu <sudhanshuj020@gmail.com>
2026-06-04 21:02:11 -0400

    another try

--- Montrial/opening-new-estate-account.component.ts
+++ Montrial/opening-new-estate-account.component.ts
@@ -5,3 +5,9 @@ tasks$: Observable<ECETaskWithAnswers[]> = combineLatest([
     this.store.select(CaseSelectors.getCaseResult)
   ]).pipe(
     map(([questions, currentStep, residence, caseData]) => {
+      this.makeFormPristine();
+      
+      // Filter out tasks by jurisdiction using the residence from the store selector
+      const filteredQuestions = this.jurisdictionFilterService.filterTasksByJurisdiction(questions || [], residence || '');
+      
+      const allQuestions: ECETaskWithAnswers[] = filteredQuestions.map(
