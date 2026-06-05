69523f9dc3d3903e89267ca31ee2b735e817805b
Sudhanshu <sudhanshuj020@gmail.com>
2026-06-04 20:40:00 -0400

    try try

--- Montrial/opening-new-estate-account.component.ts
+++ Montrial/opening-new-estate-account.component.ts
@@ -1,58 +1,30 @@
-export enum OpeningNewEstateAccountQuestionKeys {
-    WILL_NEW_ESTATE_ACCOUNT_BE_OPENED = 'willNewEstateAccountBeOpened',
-    AT_LEAST_ONE_EXECUTOR = 'atLeastOneExecutorOrLiquidatorPresent',
-    // ADD THESE:
-    EST_OF_IS_TASKED = 'estOfIstasked',
-    CHANGE_DECEASED_ADDRESS = 'changeDeceasedAddress',
-    EXECUTOR_OR_LIQUIDATOR_EXISTS = 'executorOrLiquidatorExists',
-    SET_UP_FROZEN_STATUS = 'setUpFrozenStatusOnEstateAccount',
-    ACCOUNT_APPLICATION_IS_VIEWABLE = 'accountApplicationAccountOpeningIsViewable',
-    REGULATORY_CUSTOMER_SUMMARY_IS_VIEWABLE = 'regulatoryCustomerSummaryReportIsViewable',
-    FORM_90450_IS_SCANNED = 'form90450IsScannedAndViewable',
-  }
+tasks$: Observable<ECETaskWithAnswers[]> = combineLatest([
+    this.store.select(TasksSelectors.getTasksResult),
+    this.taskConfig.currentStep$,
+  ]).pipe(
+    map(([questions, currentStep]) => {
+      this.makeFormPristine();
+      this.allQuestions = questions || [];
   
+      if (this.allQuestions && this.allQuestions.length > 0) {
+        // FIX: Match on either the step index OR pass qualifiers through safely on Step 1
+        this.stepQuestions = this.allQuestions.filter(
+          (q) => q.step === currentStep || (currentStep === 1 && q.type === 'qualifier')
+        );
   
+        this.stepQuestions.forEach((q) => {
+          const matchingAnswer = this.answers?.find(
+            (a) => a.questionId === q.id
+          );
+          if (matchingAnswer) {
+            q.answer = matchingAnswer.answer;
+          }
+        });
   
-  taskQuestions: [
-    {
-      questionKey: OpeningNewEstateAccountQuestionKeys.WILL_NEW_ESTATE_ACCOUNT_BE_OPENED,
-      answers: [...yesNoAnswers],
-      step: 1,
-    },
-    {
-      questionKey: OpeningNewEstateAccountQuestionKeys.AT_LEAST_ONE_EXECUTOR,
-      answers: [...yesNoAnswers],
-      step: 1,
-      showQuestion: false,
-    },
-    // ADD THESE 7:
-    {
-      questionKey: OpeningNewEstateAccountQuestionKeys.EST_OF_IS_TASKED,
-      step: 1,
-    },
-    {
-      questionKey: OpeningNewEstateAccountQuestionKeys.CHANGE_DECEASED_ADDRESS,
-      step: 1,
-    },
-    {
-      questionKey: OpeningNewEstateAccountQuestionKeys.EXECUTOR_OR_LIQUIDATOR_EXISTS,
-      step: 1,
-    },
-    {
-      questionKey: OpeningNewEstateAccountQuestionKeys.SET_UP_FROZEN_STATUS,
-      step: 1,
-    },
-    {
-      questionKey: OpeningNewEstateAccountQuestionKeys.ACCOUNT_APPLICATION_IS_VIEWABLE,
-      step: 1,
-    },
-    {
-      questionKey: OpeningNewEstateAccountQuestionKeys.REGULATORY_CUSTOMER_SUMMARY_IS_VIEWABLE,
-      step: 1,
-    },
-    {
-      questionKey: OpeningNewEstateAccountQuestionKeys.FORM_90450_IS_SCANNED,
-      step: 1,
-    },
-  ],
+        this.createForm(this.stepQuestions);
+        return this.stepQuestions;
+      }
   
+      return [];
+    })
+  );
