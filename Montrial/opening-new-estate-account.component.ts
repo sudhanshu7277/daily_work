4f6885d8efca7cf84ba005c52782a151a72af7b0
Sudhanshu <sudhanshuj020@gmail.com>
2026-06-04 20:51:17 -0400

    another try

--- Montrial/opening-new-estate-account.component.ts
+++ Montrial/opening-new-estate-account.component.ts
@@ -1,30 +1,7 @@
 tasks$: Observable<ECETaskWithAnswers[]> = combineLatest([
     this.store.select(TasksSelectors.getTasksResult),
     this.taskConfig.currentStep$,
+    this.store.select(CaseSelectors.getCaseResidenceOnDOD),
+    this.store.select(CaseSelectors.getCaseResult)
   ]).pipe(
-    map(([questions, currentStep]) => {
-      this.makeFormPristine();
-      this.allQuestions = questions || [];
-  
-      if (this.allQuestions && this.allQuestions.length > 0) {
-        // FIX: Match on either the step index OR pass qualifiers through safely on Step 1
-        this.stepQuestions = this.allQuestions.filter(
-          (q) => q.step === currentStep || (currentStep === 1 && q.type === 'qualifier')
-        );
-  
-        this.stepQuestions.forEach((q) => {
-          const matchingAnswer = this.answers?.find(
-            (a) => a.questionId === q.id
-          );
-          if (matchingAnswer) {
-            q.answer = matchingAnswer.answer;
-          }
-        });
-  
-        this.createForm(this.stepQuestions);
-        return this.stepQuestions;
-      }
-  
-      return [];
-    })
-  );
+    map(([questions, currentStep, residence, caseData]) => {
