9f07973867c5c8e84815b2aafb79a5e74f20119e
Sudhanshu <sudhanshuj020@gmail.com>
2026-07-05 14:56:19 -0400

    fixed legal hold payload

---
+++ Montrial/fixed.apply-legalhold-payload.ts
@@ -0,0 +1,35 @@
+{
+    "originatorData": {
+      "country": "CAD",
+      "channel": "LHUI",
+      "locationType": "workstation number",
+      "locationId": "OCT132643",
+      "applicationCatalogueId": "125846",
+      "employeeUserId": "tnoymsn",
+      "transitNumber": "string"
+    },
+    "legalHoldAction": "Apply",
+    "profiles": [
+      {
+        "profileName": "string",
+        "identifier": [
+          {
+            "idType": "string",
+            "idValue": "string"
+          }
+        ]
+      }
+    ],
+    "actionedBy": "tnoymsn",
+    "holdApplyDateTime": "2026-06-26T14:23:59.395Z",
+    "customerLifecycleStatus": "string",
+    "holdReleaseDateTime": "2026-06-26",
+    "holdsIdPk": "string",
+    "eDiscoveryProjectManager": "string",
+    "roleType": "string",
+    "holdLastUpdatedDateTime": "2026-06-26T14:23:59.395Z",
+    "holdId": "string",
+    "responsibleLawyerEmail": "string",
+    "holdType": "Legal Hold",
+    "eDiscoveryProjectManagerName": "string"
+  }
