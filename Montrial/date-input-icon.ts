464faec5294b499a4128fea11e5b624a524304bf
Sudhanshu <sudhanshuj020@gmail.com>
2026-07-18 13:11:51 -0400

    select all,

--- Montrial/date-input-icon.ts
+++ Montrial/date-input-icon.ts
@@ -107,18 +107,19 @@ isAllColumnsSelected(): boolean {
   /* Target standard toggleable dropdown items when they are active/selected */
 /* Target standard toggleable dropdown items */
 
+/* Target standard toggleable dropdown items */
 .mat-mdc-option:not(.select-all-option) {
 
-    /* 1. MANDATORY ITEMS: Explicitly targeting rows with the disabled attribute */
-    &[disabled] {
+    /* 1. MANDATORY ITEMS: Targeted via the explicit disabled class name */
+    &.mat-mdc-option-disabled {
       .mat-pseudo-checkbox-full {
         background-color: #707070 !important;
         border-color: #707070 !important;
       }
     }
   
-    /* 2. OPTIONAL ITEMS: Explicitly targeting active rows that do NOT have the disabled attribute */
-    &:not([disabled]) {
+    /* 2. OPTIONAL ITEMS: Target active rows that do NOT have the disabled class */
+    &:not(.mat-mdc-option-disabled) {
       &.mdc-list-item--selected {
         .mat-pseudo-checkbox-full {
           background-color: $bmo-blue !important;
@@ -127,5 +128,3 @@ isAllColumnsSelected(): boolean {
       }
     }
   }
-  
-  /* 🛑 LINE 513 REMOVED ENTIRELY TO PREVENT THE OVERRIDE FILTER LEAK */
