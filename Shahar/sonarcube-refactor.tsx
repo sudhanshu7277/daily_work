e086c29692cfed4aed3057a4f760bf3fd212325a
Sudhanshu <sudhanshuj020@gmail.com>
2026-08-05 10:54:48 -0400

    sonar cube

--- Shahar/sonarcube-refactor.tsx
+++ Shahar/sonarcube-refactor.tsx
@@ -1,3 +1,8 @@
 //In VerifyPaymentDetailModal.tsx at line 33:
 
 function SpreadsheetPreview({ url, fileType }: { url: string; fileType: string }) {
+
+
+  //Updated Line 50:
+
+  if (!cancelled) { setWb(book); setRows(data); }
