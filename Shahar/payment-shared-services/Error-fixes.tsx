a1ccb1a97350e340323955d7cf8f7bfc30d18f68
Sudhanshu <sudhanshuj020@gmail.com>
2026-08-30 21:50:09 -0400

    syntax fixes 08

--- Shahar/payment-shared-services/Error-fixes.tsx
+++ Shahar/payment-shared-services/Error-fixes.tsx
@@ -1,6 +1,2 @@
-//Option 1: Hard Reset (Cleanest)
-Resets your local and remote branch directly back to the working green commit:
-
-
-git reset --hard 8f984ab
-git push origin feature/IS-74377-Integration-maker-file-viewer --force-with-lease
+git commit --allow-empty -m "chore: trigger CI build for IS-74377"
+git push origin feature/IS-74377-Integration-maker-file-viewer
